6bbc2bc03df0278c322ffa70fb956befb265ac4d
Johannes Bloch <johannes.bloch@gmail.com>
2026-02-11 08:10:52 +0100

    emove KafkaOptions, standardize namespaces, cleanup global usings

--- src/JohBloch.ConfluentKafka.Clients.Core/Interfaces/IMessageDeserializer.cs
+++ src/JohBloch.ConfluentKafka.Clients.Core/Interfaces/IMessageDeserializer.cs
@@ -1,17 +1,16 @@
-namespace JohBloch.ConfluentKafka.Clients.Interfaces
+namespace JohBloch.ConfluentKafka.Clients.Interfaces;
+
+/// <summary>
+/// Generic interface for deserializing messages from different schema formats.
+/// </summary>
+/// <typeparam name="T">The target type to deserialize into.</typeparam>
+public interface IMessageDeserializer<T>
 {
     /// <summary>
-    /// Generic interface for deserializing messages from different schema formats.
+    /// Deserializes a byte array into the specified type using the appropriate schema format.
     /// </summary>
-    /// <typeparam name="T">The target type to deserialize into.</typeparam>
-    public interface IMessageDeserializer<T>
-    {
-        /// <summary>
-        /// Deserializes a byte array into the specified type using the appropriate schema format.
-        /// </summary>
-        /// <param name="data">The raw message bytes to deserialize.</param>
-        /// <param name="context">Serialization context containing topic and message component information.</param>
-        /// <returns>The deserialized message of type T.</returns>
-        Task<T> DeserializeAsync(byte[] data, SerializationContext context);
-    }
+    /// <param name="data">The raw message bytes to deserialize.</param>
+    /// <param name="context">Serialization context containing topic and message component information.</param>
+    /// <returns>The deserialized message of type T.</returns>
+    Task<T> DeserializeAsync(byte[] data, SerializationContext context);
 }
--- src/JohBloch.ConfluentKafka.Clients.Core/Services/Serialization/SchemaRegistryWireFormat.cs
+++ src/JohBloch.ConfluentKafka.Clients.Core/Services/Serialization/SchemaRegistryWireFormat.cs
@@ -1,65 +1,64 @@
-namespace JohBloch.ConfluentKafka.Clients.Services.Serialization
+namespace JohBloch.ConfluentKafka.Clients.Services.Serialization;
+
+/// <summary>
+/// Constants and utilities for Confluent Schema Registry wire format.
+/// </summary>
+public static class SchemaRegistryWireFormat
 {
     /// <summary>
-    /// Constants and utilities for Confluent Schema Registry wire format.
+    /// Magic byte that indicates Schema Registry wire format (0x00).
     /// </summary>
-    public static class SchemaRegistryWireFormat
+    public const byte MagicByte = 0x00;
+
+    /// <summary>
+    /// Minimum length of Schema Registry wire format message (magic byte + 4-byte schema ID).
+    /// </summary>
+    public const int MinimumMessageLength = 5;
+
+    /// <summary>
+    /// Offset where payload data begins after Schema Registry header (after magic byte + schema ID).
+    /// </summary>
+    public const int PayloadOffset = 5;
+
+    /// <summary>
+    /// Checks if data has Schema Registry wire format.
+    /// </summary>
+    /// <param name="data">The message data to check.</param>
+    /// <returns>True if data has Schema Registry wire format, false otherwise.</returns>
+    public static bool HasWireFormat(byte[] data)
     {
-        /// <summary>
-        /// Magic byte that indicates Schema Registry wire format (0x00).
-        /// </summary>
-        public const byte MagicByte = 0x00;
+        return data.Length >= MinimumMessageLength && data[0] == MagicByte;
+    }
 
-        /// <summary>
-        /// Minimum length of Schema Registry wire format message (magic byte + 4-byte schema ID).
-        /// </summary>
-        public const int MinimumMessageLength = 5;
+    /// <summary>
+    /// Extracts the payload from Schema Registry wire format data.
+    /// If data doesn't have wire format, returns the original data.
+    /// </summary>
+    /// <param name="data">The message data.</param>
+    /// <returns>The payload without Schema Registry header.</returns>
+    public static byte[] ExtractPayload(byte[] data)
+    {
+        return HasWireFormat(data) ? data[PayloadOffset..] : data;
+    }
 
-        /// <summary>
-        /// Offset where payload data begins after Schema Registry header (after magic byte + schema ID).
-        /// </summary>
-        public const int PayloadOffset = 5;
-
-        /// <summary>
-        /// Checks if data has Schema Registry wire format.
-        /// </summary>
-        /// <param name="data">The message data to check.</param>
-        /// <returns>True if data has Schema Registry wire format, false otherwise.</returns>
-        public static bool HasWireFormat(byte[] data)
+    /// <summary>
+    /// Extracts the schema ID from Schema Registry wire format data.
+    /// </summary>
+    /// <param name="data">The message data with Schema Registry wire format.</param>
+    /// <returns>The schema ID in big-endian format.</returns>
+    /// <exception cref="InvalidDataException">If data doesn't have Schema Registry wire format.</exception>
+    public static int ExtractSchemaId(byte[] data)
+    {
+        if (!HasWireFormat(data))
         {
-            return data.Length >= MinimumMessageLength && data[0] == MagicByte;
+            throw new InvalidDataException("Data does not have Schema Registry wire format");
         }
 
-        /// <summary>
-        /// Extracts the payload from Schema Registry wire format data.
-        /// If data doesn't have wire format, returns the original data.
-        /// </summary>
-        /// <param name="data">The message data.</param>
-        /// <returns>The payload without Schema Registry header.</returns>
-        public static byte[] ExtractPayload(byte[] data)
+        // Schema ID is bytes 1-4 in big-endian format
+        if (BitConverter.IsLittleEndian)
         {
-            return HasWireFormat(data) ? data[PayloadOffset..] : data;
+            return (data[1] << 24) | (data[2] << 16) | (data[3] << 8) | data[4];
         }
-
-        /// <summary>
-        /// Extracts the schema ID from Schema Registry wire format data.
-        /// </summary>
-        /// <param name="data">The message data with Schema Registry wire format.</param>
-        /// <returns>The schema ID in big-endian format.</returns>
-        /// <exception cref="InvalidDataException">If data doesn't have Schema Registry wire format.</exception>
-        public static int ExtractSchemaId(byte[] data)
-        {
-            if (!HasWireFormat(data))
-            {
-                throw new InvalidDataException("Data does not have Schema Registry wire format");
-            }
-
-            // Schema ID is bytes 1-4 in big-endian format
-            if (BitConverter.IsLittleEndian)
-            {
-                return (data[1] << 24) | (data[2] << 16) | (data[3] << 8) | data[4];
-            }
-            return BitConverter.ToInt32(data, 1);
-        }
+        return BitConverter.ToInt32(data, 1);
     }
 }
